8f0762ad5ca3b7ba6f7063f4b9e16c92d8160b66
bhatshakir11 <b.shakir8491@gmail.com>
2026-02-14 17:58:48 +0530

    added new files

--- labprograms/code1.java
+++ labprograms/code1.java
@@ -8,7 +8,7 @@ public class code1{
         String str2=sc.nextLine();
         for(int i=0;i<str.length()&&i<str2.length();i++){
             System.out.print(str.charAt(i));
-            System.out.print(str2.charAt(i));
+            
         }
         
     }
